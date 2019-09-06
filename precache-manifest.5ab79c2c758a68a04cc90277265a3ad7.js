4793be70fa9bbe546991bc7f5c01947dc8464720
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2019-09-06 15:08:24 -0500

    Updates

---
+++ precache-manifest.5ab79c2c758a68a04cc90277265a3ad7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1166af507c0b55e18758",
+    "url": "/Askisers-Class-Participation/static/js/runtime~main.1166af50.js"
+  },
+  {
+    "revision": "6ae178f8896b39b42048",
+    "url": "/Askisers-Class-Participation/static/js/main.6ae178f8.chunk.js"
+  },
+  {
+    "revision": "1e5545e72055d874761d",
+    "url": "/Askisers-Class-Participation/static/js/2.1e5545e7.chunk.js"
+  },
+  {
+    "revision": "6ae178f8896b39b42048",
+    "url": "/Askisers-Class-Participation/static/css/main.bdb44df8.chunk.css"
+  },
+  {
+    "revision": "c30464699e8601f1b9a656113c0e69a9",
+    "url": "/Askisers-Class-Participation/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/askisers-react/precache-manifest.ac1ada4e11cdc16b6e965892fa5e8faa.js"
+  "/Askisers-Class-Participation/precache-manifest.5ab79c2c758a68a04cc90277265a3ad7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/askisers-react/index.html", {
+workbox.routing.registerNavigationRoute("/Askisers-Class-Participation/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
